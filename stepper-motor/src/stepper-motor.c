836291f994c2c904e096ff3cb4b5d57842666696
Zycrus <47321670+zycrus@users.noreply.github.com>
2024-11-03 14:49:32 +0800

    Update stepper-motor.c

--- stepper-motor/src/stepper-motor.c
+++ stepper-motor/src/stepper-motor.c
@@ -1,1 +1,15 @@
 //
+double DegreesToPercent(double _deg)
+{
+    return _deg/180;
+}
+
+/*
+    0deg - 1ms
+    90deg - 1.5ms
+    180deg - 2ms
+*/
+double PercentToPulse(double _deg, double _time)
+{
+    return (DegreesToPercent(_deg) + 1);
+}
